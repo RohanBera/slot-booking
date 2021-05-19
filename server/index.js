ff017ae13c5832ebb9a8169f619fd10a3ce862d8
RohanBera <berarohan88@gmail.com>
2021-05-19 12:00:51 +0530

    build changes

--- server/index.js
+++ server/index.js
@@ -27,12 +27,11 @@ app.use("/entry", DataRouter);
 //dates routes 
 app.use("/date", DateRouter);
 
-// app.use(express.static(path.resolve(__dirname,'../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World!" });
+// SERVING
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
-
-// app.get('/', (req,res) => {
-//     res.sendFile(path.resolve(__dirname,'../client/build','index.html'));
-// });
